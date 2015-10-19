c5bc507cf7f6298212fde32d7a07309685cde4d0
Christian Fei <crifei93@gmail.com>
2015-10-19 21:52:17 +0200

    refactoring tests

--- test/elixir_unix_pipe_example_test.exs
+++ test/elixir_unix_pipe_example_test.exs
@@ -2,30 +2,26 @@ defmodule UnixTest do
   use ExUnit.Case
   doctest Unix
 
-  test "#ps returns operating system processes" do
-    assert Unix.ps() == """
+  @processes """
   PID TTY           TIME CMD
   494 ttys000    0:00.42 -/usr/local/bin/fish
  4369 ttys000    0:00.62 npm
  4374 ttys000    0:00.00 sh -c gulp clean && gulp watch
  4381 ttys000    1:57.02 gulp
  9728 ttys001    0:00.79 -/usr/local/bin/fish
-    """
+  """
+
+  test "#ps returns operating system processes" do
+    assert Unix.ps() == @processes
   end
 
   test "#grep filters lines by match" do
-    input = """
-foo
-bar
-baz
-    """
     match = ~r/ba/
-    assert Unix.grep(input, match) == ["bar","baz"]
+    assert Unix.grep("foo\nbar\nbaz", match) == ["bar","baz"]
   end
 
   test "#awk get specified column" do
-    input = ["foo bar"]
     output = ["bar"]
-    assert Unix.awk(input, 1) == output
+    assert Unix.awk(["foo bar"], 1) == output
   end
 end
