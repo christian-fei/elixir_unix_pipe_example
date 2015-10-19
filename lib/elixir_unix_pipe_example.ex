84db4d355ac42db6b5c5041f805f54fc4c47cb40
Christian Fei <crifei93@gmail.com>
2015-10-19 21:48:33 +0200

    using attribute

--- lib/elixir_unix_pipe_example.ex
+++ lib/elixir_unix_pipe_example.ex
@@ -1,16 +1,18 @@
 defmodule Unix do
-  @doc """
-    Returns operating system processes
-  """
-  def ps do
-    """
+  @processes """
   PID TTY           TIME CMD
   494 ttys000    0:00.42 -/usr/local/bin/fish
  4369 ttys000    0:00.62 npm
  4374 ttys000    0:00.00 sh -c gulp clean && gulp watch
  4381 ttys000    1:57.02 gulp
  9728 ttys001    0:00.79 -/usr/local/bin/fish
-    """
+  """
+
+  @doc """
+    Returns operating system processes
+  """
+  def ps do
+    @processes
   end
 
   @doc ~S"""
